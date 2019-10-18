a4d294db4cc3aabc3254e60f1990d6522de531b7
Anand Kumar Prajapati <anapraja@publicisgroupe.net>
2019-10-18 16:21:03 +0530

    initial code base

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const fs = require('fs');
+// const UUID = require('uuid/v1');
+
+const app = express();
+let brandData = JSON.parse(fs.readFileSync('./data/brand_image.json'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('./assets'));
+
+app.post('/api/setImageStandard', async (req, res) => {
+    let _reqdata = req.body;
+    if (brandData.filter(t => { return t.brand.toLowerCase() === _reqdata.brand.toLowerCase(); }).length === 0) {
+        brandData.push(_reqdata);
+        console.log("new record found");
+        
+    }
+    fs.writeFileSync('./data/brand_image.json', JSON.stringify(brandData));
+    res.send("Done");
+});
+
+app.get('/api/getImageStandard/:brandName', (req, res) => {
+    let _bdata = brandData.filter(t => {
+        return t.brand.toLowerCase() === req.params.brandName.toLowerCase();
+    });
+    res.send(_bdata[0].standards);
+});
+
+app.listen(process.env.port || process.env.PORT || 3000);
+console.log(brandData);
